823db621b5a1298949391652751ec14696101ec2
Miguel Felix <mikeyfelix99@gmail.com>
2020-07-10 12:18:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,4 +8,10 @@ def countdown (number)
   puts "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep (number)
+   while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+end
